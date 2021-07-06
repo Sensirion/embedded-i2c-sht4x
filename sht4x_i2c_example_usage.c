3b3f1646310f738670870f48b829710f688529cb
Bjoern Muntwyler <Bjoern.Muntwyler@sensirion.com>
2021-07-06 13:31:26 +0200

    Add generator output

---
+++ sht4x_i2c_example_usage.c
@@ -0,0 +1,67 @@
+/*
+ * I2C-Generator: 0.2.0
+ * Yaml Version: 0.1.0
+ * Template Version: 0.7.0-12-g6411a7e
+ */
+/*
+ * Copyright (c) 2021, Sensirion AG
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Sensirion AG nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdio.h>  // printf
+
+#include "sensirion_common.h"
+#include "sensirion_i2c_hal.h"
+#include "sht4x_i2c.h"
+
+/*
+ * TO USE CONSOLE OUTPUT (PRINTF) YOU MAY NEED TO ADAPT THE INCLUDE ABOVE OR
+ * DEFINE IT ACCORDING TO YOUR PLATFORM:
+ * #define printf(...)
+ */
+
+// TODO: DRIVER_GENERATOR Add missing commands and make prints more pretty
+
+int main(void) {
+    int16_t error = 0;
+
+    sensirion_i2c_hal_init();
+
+    // Start Measurement
+
+    for (;;) {
+        // Read Measurement
+        // TODO: DRIVER_GENERATOR check and update measurement interval
+        sensirion_i2c_hal_sleep_usec(1000000);
+        // TODO: DRIVER_GENERATOR Add scaling and offset to printed measurement
+        // values
+    }
+
+    return 0;
+}
